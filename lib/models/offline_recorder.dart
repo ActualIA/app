8575fd9d548625c4669752135b96cd38f1af7eef
Roman Paccaud <roman.paccaud@epfl.ch>
2024-04-29 02:24:22 +0200

    feat: Add Serialization and Deserialization as well as tests

--- lib/models/offline_recorder.dart
+++ lib/models/offline_recorder.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
-import 'dart:path_provider';
+import 'package:actualia/models/news.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'package:actualia/viewmodels/news.dart';
 
@@ -7,7 +8,7 @@ import 'package:actualia/viewmodels/news.dart';
  * Records news locally to retrieve them offline. Must be created through OfflineRecorder.create(newsProvider)
  */
 class OfflineRecorder {
-  static final ROOT_OFFLINE_FOLDER = "offlineNews";
+  static const ROOT_OFFLINE_FOLDER = "offlineNews";
 
   late final _newsProvider;
   late final _appOfflineNewsPath;
@@ -31,5 +32,45 @@ class OfflineRecorder {
     }
 
     return OfflineRecorder._create(newsProvider, appOfflineNewsPath);
+  }
+
+  static Map<String, dynamic> serializeNews(News news) {
+    List<Map<String, String>> paragraphs = [];
+    for (Paragraph p in news.paragraphs) {
+      paragraphs.add({
+        "title": p.title,
+        "date": p.date,
+        "content": p.content,
+        "source": p.source
+      });
+    }
+
+    Map<String, String> serializedNews = {
+      "title": news.title.toString(),
+      "date": news.date.toString(),
+      "transcriptID": news.transcriptID.toString(),
+      "paragraphs": paragraphs.toString(),
+    };
+
+    return serializedNews;
+  }
+
+  static News deserializeNews(Map<String, dynamic> storedNews) {
+    List<Paragraph> paragraphs = List.empty();
+    for (Map<String, String> p in storedNews["paragraphs"]) {
+      paragraphs.add(Paragraph(
+          transcript: storedNews["transcriptID"],
+          source: p["source"]!,
+          title: p["title"]!,
+          date: p["date"]!,
+          content: p["content"]!));
+    }
+
+    return News(
+        audio: "",
+        title: storedNews["title"],
+        date: storedNews["date"],
+        transcriptID: int.parse(storedNews["transcriptID"]),
+        paragraphs: paragraphs);
   }
 }
---
+++ test/unit/offline_recoreder.dart
@@ -0,0 +1,38 @@
+import 'package:actualia/models/news.dart';
+import 'package:actualia/models/offline_recorder.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+News testNews = News(
+    title: "Test title",
+    date: "17/10/2002",
+    transcriptID: 0,
+    audio: "./audio/test0",
+    paragraphs: [
+      Paragraph(
+          transcript: "0",
+          title:
+              "Sweet Victory: Local Bakery Garners National Accolades for Inventive Cupcake Creations",
+          date: "17/10/2002",
+          content:
+              "Local bakery wins national award for innovative cupcake flavors, residents celebrate with free tastings. Mayor commends efforts, hails bakery as symbol of community creativity and entrepreneurship.",
+          source:
+              "This news comes from \"The Daily Gazette,\" a renowned source for community updates and local achievements, highlighting the innovative cupcake flavors and celebratory atmosphere at a nearby bakery."),
+      Paragraph(
+          transcript: "0",
+          title:
+              "Groundbreaking Study Reveals Surprising Benefits of Meditation on Workplace Productivity",
+          date: "17/10/2002",
+          content:
+              "In a groundbreaking study published by \"The Mindful Worker,\" researchers unveil unexpected advantages of meditation in boosting workplace productivity. Findings suggest that regular meditation practices can significantly enhance focus and efficiency.",
+          source:
+              "This news comes from \"The Mindful Worker,\" a leading publication dedicated to exploring the intersection of mindfulness and professional success."),
+    ]);
+
+void main() {
+  test("Correct Serialisation and Deserialization", () {
+    expect(
+        OfflineRecorder.deserializeNews(
+            OfflineRecorder.serializeNews(testNews)),
+        equals(testNews));
+  });
+}
