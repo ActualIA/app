ec3ae4793e64d1229ae63cc63b68ea2bd2fb26dc
Paul Madelénat <18498650+pmdlt@users.noreply.github.com>
2024-05-26 16:02:10 +0000

    feat: add fetchRawNewsList()

--- lib/viewmodels/rss_feed.dart
+++ lib/viewmodels/rss_feed.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:actualia/models/article.dart';
 import 'package:actualia/models/news.dart';
 import 'package:flutter/material.dart';
@@ -5,8 +7,16 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class RSSFeedViewModel extends ChangeNotifier {
   late final List<Article> articles;
+  late final SupabaseClient supabase;
 
-  // TODO: fill.
+  Future<List<dynamic>> fetchRawNewsList() async {
+    try {
+      final res = await supabase.functions.invoke('generate-raw-feed');
+      return res.data as List<News>;
+    } catch (e) {
+      throw Exception("Failed to invoke generate-raw-feed function");
+    }
+  }
 
   RSSFeedViewModel(this.articles);
 }
